3ccbe896303935d535612ab514b4ad0bd2566e39
Ekaterina Ponomarenko <naamah-K@ya.ru>
2017-12-22 18:55:42 +0300

    поправила Б20

--- js/show-card.js
+++ js/show-card.js
@@ -1,5 +1,7 @@
 'use strict';
-window.showCard = function (data) {
-  window.card.renderCard(data);
-  window.card.showPopup();
-};
+(function () {
+  window.showCard = function (data) {
+    window.card.renderCard(data);
+    window.card.showPopup();
+  };
+})();
